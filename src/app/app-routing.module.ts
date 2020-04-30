82b119fb025396c721cf31c06dd58ebe8df6b92f
quantumpanic <quantumpanic@gmail.com>
2020-04-30 16:19:39 +0700

    redirect ke register

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", redirectTo: "register", pathMatch: "full"},
+  {path: "register", component: AppComponent},
+  {path: "**", redirectTo: "register"},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
